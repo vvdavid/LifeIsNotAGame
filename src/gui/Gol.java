61d0ab18dc7a99ff583439713ac9a9734c7c1342
David Vazquez <vvdavided@gmail.com>
2018-12-13 21:46:32 -0600

    Add Gol class

---
+++ src/gui/Gol.java
@@ -0,0 +1,65 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import objects.Paint;
+
+public class Gol extends javax.swing.JPanel implements Runnable, Paint {
+
+    private final boolean[][] cells;
+    private final int cellLength;
+    private boolean living = true;
+
+    public Gol(Dimension size, boolean[][] cells, int cellLength) {
+        initComponents();
+        setSize(size);
+        this.cells = cells;
+        this.cellLength = cellLength;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        while (living) {
+            reproduce();
+            sleep(1000 / 60);
+            repaint();
+        }
+    }
+
+    private void reproduce() {
+        
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        paintBackground(g, getWidth(), getHeight());
+        paintCells(g, cells, ALLBITS);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+    private void sleep(int milis) {
+        try {
+            Thread.sleep(milis);
+        } catch (InterruptedException e) {
+            System.err.println(e);
+        }
+    }
+
+}
