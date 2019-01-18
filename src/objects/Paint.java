7f33618e03cab902485456a581cc17b451c7c55d
David Vazquez <vvdavided@gmail.com>
2019-01-17 18:37:10 -0600

    Use pastell colors to paint cells

--- src/objects/Paint.java
+++ src/objects/Paint.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.UIManager;
 
 public interface Paint {
@@ -12,10 +13,20 @@ public interface Paint {
 
     public default void paintBackground(Graphics g, int width, int heigth) {
 //        g.setColor(c);
-        g.setColor(Color.WHITE);
+        g.setColor(Color.BLACK);
         g.fillRect(0, 0, width, heigth);
     }
 
+    /**
+     * Paints the given cells 2D array with pastell colors using the Graphics object.
+     * Has the ability to draw a gap between every cell
+     * Cell size specified in pixels
+     *
+     * @param g
+     * @param cells
+     * @param cellLength
+     * @param gap
+     */
     public default void paintCells(Graphics g, boolean[][] cells, int cellLength, int gap) {
         ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
@@ -25,8 +36,25 @@ public interface Paint {
         for (boolean[] row : cells) {
             for (boolean cell : row) {
                 if (cell) {
-                    g.setColor(Color.RED);
+//                    g.setColor(Color.CYAN);
+                    g.setColor(new Color(
+                            ThreadLocalRandom.current().nextInt(0, 128) + 127,
+                            ThreadLocalRandom.current().nextInt(0, 128) + 127,
+                            ThreadLocalRandom.current().nextInt(0, 128) + 127
+                    ));
                     g.fillOval(actualX, actualY, cellLength, cellLength);
+//                    g.setColor(Color.YELLOW);
+                    g.setColor(new Color(
+                            ThreadLocalRandom.current().nextInt(0, 128) + 127,
+                            ThreadLocalRandom.current().nextInt(0, 128) + 127,
+                            ThreadLocalRandom.current().nextInt(0, 128) + 127
+                    ));
+                    g.fillOval(
+                            actualX + (cellLength / 4),
+                            actualY + (cellLength / 4),
+                            cellLength / 2,
+                            cellLength / 2);
+//                    g.drawOval(4, actualY * 1.5, cellLength * .5, cellLength * .5);
                 }
                 actualX += cellLength + gap;
             }
